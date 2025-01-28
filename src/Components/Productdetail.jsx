1d795b8dd4725683fa4e4758c6901ff8e96ca700
rojass2001 <rojascherian@gmail.com>
2025-01-28 22:46:34 +0530

    second commit

--- src/Components/Productdetail.jsx
+++ src/Components/Productdetail.jsx
@@ -9,7 +9,7 @@ function Productdetail() {
   return (
 <>
     <div className='w-full min-h-screen overflow-y-auto place-content-center absolute top-1 justify-center  place-items-center sm:px-1'>
-      <div className=' w-[50%] min-h-[300px] overflow-y-auto  p-2 relative flex gap-3 mx-auto bg-red-300 shadow-md shadow-gray-400 sm:flex-col sm:w-full  md:h-[380px] md:w-[70%]  '>
+      <div className=' w-[50%] min-h-[300px] overflow-y-auto  p-2 relative flex gap-3 mx-auto shadow-md shadow-gray-400 sm:flex-col sm:w-full  md:h-[380px] md:w-[70%]  '>
         <p onClick={()=>dispatch(productpopup())} className=' text-2xl font-bold absolute top-1
          right-3 sm:top-0 cursor-pointer bg-white'>x</p>
         <div className='h-[300px] w-[50%] sm:w-full'>
@@ -26,7 +26,7 @@ function Productdetail() {
                 <button onClick={()=>dispatch(increase())} className='w-16 border-[1px] h-9 border-gray-300 ml-2'>+</button>
               </div>
               <div className='flex gap-2 mt-4 sm:gap-4 text-center place-items-center justify-center'>
-                <button onClick={()=>dispatch(addtocart(productdetails))} className='w-[120px] h-10 bg-red-400 '>Addtocart</button>
+                <button onClick={()=>dispatch(addtocart(productdetails))} className='w-[120px] h-10 bg-orange-400 '>Addtocart</button>
                 <button className='w-[120px] h-10 bg-orange-400 '>PayNow${productdetails.price*productdetails.quantity}</button>
               </div>
             </div>
