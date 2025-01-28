9e4fc2c936cb9c427d7ae7d869ac7be14b4d28c4
rojass2001 <rojascherian@gmail.com>
2025-01-28 23:11:32 +0530

    second commit

--- src/Components/Productdetail.jsx
+++ src/Components/Productdetail.jsx
@@ -9,11 +9,11 @@ function Productdetail() {
   return (
 <>
     <div className='w-full min-h-screen overflow-y-auto place-content-center absolute top-1 justify-center  place-items-center sm:px-1'>
-      <div className=' w-[50%] min-h-[300px] overflow-y-auto  p-2 relative flex gap-3
+      <div className=' w-[50%] min-h-[300px] overflow-y-auto   relative flex gap-3
        mx-auto shadow-md bg-white shadow-gray-400 sm:flex-col sm:w-full  md:h-[380px] md:w-[70%]  '>
         <p onClick={()=>dispatch(productpopup())} className=' text-2xl font-bold absolute top-1
          right-3 sm:top-0 cursor-pointer bg-white'>x</p>
-        <div className='h-[300px] w-[50%] sm:w-full'>
+        <div className='h-[300px]  w-[50%] sm:h-[150px] sm:w-full'>
           <img className='h-full w-full ' src={productdetails?.image}/>
         </div>
            <div className='w-[60%] sm:w-full sm:text-center sm:place-items-center font-bold text-black pt-3'>
@@ -21,7 +21,7 @@ function Productdetail() {
               <p >{productdetails?.des}</p>
               <p className=' text-xl '>${productdetails?.price}</p>
               <p className='text-green-400 text-sm'>{productdetails.stocks}</p>
-              <div className='flex items-center '>
+              <div className='flex w-full justify-center place-items-center items-center '>
                 <button onClick={()=>dispatch(decrease())} className='w-16 border-[1px] h-9 border-gray-300 mr-2'>-</button>
                 {productdetails.quantity}
                 <button onClick={()=>dispatch(increase())} className='w-16 border-[1px] h-9 border-gray-300 ml-2'>+</button>
