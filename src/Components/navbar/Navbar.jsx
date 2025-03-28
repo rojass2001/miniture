049afe51dd581dcbf575bce25d66e87fae39f43a
rojass2001 <rojascherian@gmail.com>
2025-03-28 17:28:52 +0530

    third commit

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -15,8 +15,8 @@ function Navbar() {
     initial={{opacity:0,y:-100}}
     animate={{opacity:1,y:0}}
     transition={{duration:0.4,delay:0.6}}
-    className="w-full fixed bg-white z-50 top-0 right-0 left-0 shadow-md shadow-gray-500  h-[50px] flex
-     items-center justify-around">
+    className="w-full fixed bg-white z-50 sm:px-2 top-0 right-0 left-0 shadow-md shadow-gray-500 h-[60px] flex
+     items-center justify-around sm:justify-between ">
       <h2 className='text-3xl font-black  mt-1  '>Miniture</h2>
       <Desktopnav/>
       {open&&
